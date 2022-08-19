240084ba2a34a84cde6b1baeb8f1fec744c9508e
U_U_JIN <yyj9434@naver.com>
2022-08-19 18:30:17 +0900

    YJ 22-08-19 spring boot / build.gradle 설정

---
+++ src/test/java/com/ouou/sooljari/SooljariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouou.sooljari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SooljariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
