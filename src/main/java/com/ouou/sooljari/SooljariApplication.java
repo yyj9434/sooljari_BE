569daa675d5bcb5981217e683935072bd418ae78
U_U_JIN <yyj9434@naver.com>
2022-08-25 14:52:56 +0900

    YJ 22-08-25 Spring Boot 기본 세팅

---
+++ src/main/java/com/ouou/sooljari/SooljariApplication.java
@@ -0,0 +1,13 @@
+package com.ouou.sooljari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SooljariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SooljariApplication.class, args);
+	}
+
+}
