beba302db732397bdc9cd290d347ae6143e3bacd
hyexrin <ohr1003@gmail.com>
2022-09-02 16:36:12 +0900

    HR 22-09-02 spring boot - react 연결 테스트용

--- src/main/java/com/ouou/sooljari/controller/HelloController.java
+++ src/main/java/com/ouou/sooljari/controller/HelloController.java
@@ -7,9 +7,13 @@ import java.util.Date;
 
 @RestController
 public class HelloController {
-
     @GetMapping("/test")
     public String time() {
         return "안녕하세요. 현재 서버의 시간은 " + new Date() + " 입니다!!!!!!";
+    }
+
+    @GetMapping("/loginTest")
+    public String loginTest() {
+        return "hello";
     }
 }
